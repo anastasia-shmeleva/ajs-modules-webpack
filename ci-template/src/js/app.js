727d80f8f3b2448d31be82704bf2fd2b58c9f2ae
Anastasia Shmeleva <shmeleva-nastya@mail.ru>
2021-06-17 12:53:18 +0300

    init

--- ci-template/src/js/app.js
+++ ci-template/src/js/app.js
@@ -1,6 +1,8 @@
 // TODO: write your code here
 import sum from './basic';
 
-console.log('worked');
+// eslint-disable-next-line no-console
+console.log('app worked');
 
+// eslint-disable-next-line no-console
 console.log(sum([1, 2]));
